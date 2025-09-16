b5db6c343a14523bc613d96c5932eda2e620b163
Rob J Goedman <goedman@icloud.com>
2025-09-16 08:13:49 -0600

    Initial Commit

---
+++ SwiftDataMediumTests/SwiftDataMediumTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftDataMediumTests.swift
+//  SwiftDataMediumTests
+//
+//  Created by Robert Goedman on 9/16/25.
+//
+
+import Testing
+
+struct SwiftDataMediumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
